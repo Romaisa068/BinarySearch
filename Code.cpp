ab04472583621b14d2eea3b830adf4f3fd29bee4
Romaisa Khanam <147841967+Romaisa068@users.noreply.github.com>
2024-01-10 15:33:30 +0500

    Update Code.cpp

--- Code.cpp
+++ Code.cpp
@@ -1,20 +1,18 @@
+// Online C++ compiler to run C++ program online
 #include<iostream>
 using namespace std;
 
-bool binarySearch(int arr[][4], int n,int m, int t){
+bool binarySearch(int arr[4], int n, int t){
     int s =0;
-    int e = (n*m)-1;
+    int e = n;
 
     int mid = s+ (e-s)/2;
 
     while(s <= e){
-        int rowIndex = mid / m;
-        int colIndex = mid % m;
-        cout<<rowIndex<<" "<<colIndex<<endl;
-        if(arr[rowIndex][colIndex] == t){
+        if(arr[mid] == t){
             return true;
         }
-        if(arr[rowIndex][colIndex] < t ){
+        if(arr[mid] < t ){
             s = mid + 1; 
         }else{
             e = mid-1;
@@ -25,33 +23,15 @@ bool binarySearch(int arr[][4], int n,int m, int t){
     return false;
 }
 
-bool binSearII(int arr[][4], int n,int m, int t){
-    int row = 0;
-    int col = m-1;
-    while(row < n && col >=0){
-        int element = arr[row][col];
-        if(element == t){
-            return true;
-        }
-        if(element < t){
-            row++;
-        }else{
-            col--;
-        }
-    }
-    return false;
-}
 
 main(){
 
-    int a[3][4] = {{1,2,3,4},
-                   {5,6,7,8},
-                   {9,10,11,12}};
+    int a[4] = {1,2,3,4};
     int target = 11;
-//  cout<<binarySearch(a,3,4,target);
-    int b[3][4] = {{1,4,7,11},
-                   {2,5,8,12},
-                   {3,6,9,16}};
-    int t = 9;
-    cout<<binSearII(b,3,4,t);             
+    if(binarySearch(a,4,target)){
+        cout<<"Element is present"<<endl;
+    }else{
+        cout<<"Element is not present!";
+    }
+            
 }
